dc369d784d456bf39dab941197151bb0f9f973c7
FlowTech <admin@FlowTech-LAB.org>
2025-10-24 00:40:22 +0200

    Enhance script_1.js to include user interaction and factorial calculation

--- script_1.js
+++ script_1.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+console.log("Hello World!");
+
+let prenom = prompt("Quel est votre prénom ?");
+console.log(`Wesh, ${prenom} !`);
+
+
+let nombre = prompt("Donne un numero ");
+let resultat = 1;
+for (let i = nombre; i >= 1; i--) {
+    resultat = resultat * i;
+}
+console.log(resultat);
